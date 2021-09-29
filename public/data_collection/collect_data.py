90f9ba64c23ebca2f47d9acdd424a711098060dc
Alex Chen <Zhenke.18@intl.zju.edu.cn>
2021-09-29 19:27:33 +0800

    fetch_json and web_clawer added

--- public/data_collection/collect_data.py
+++ public/data_collection/collect_data.py
@@ -13,13 +13,15 @@ import urllib.parse
 import requests
 import ssl
 import json
-from bs4 import BeautifulStoneSoup
+from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
+
 
 # define the module level constants
 STRING_LENGTH = 5
 DOC = "tmp_website.html"
 TIME_OUT = 60
+
 
 # parameters setting for the Google Programmable Search Engine, which can be modified based on different users
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
@@ -46,3 +48,52 @@ def google_search( question, result_num = 2):
     json_content = json.loads(content.decode("utf8"))
 
     return json_content
+
+
+def fetch_json( json_content ):
+
+    '''
+        Fetch the list of websites from the json content
+
+        Keyword arguments:
+        json_content -- json content from API
+    '''
+
+    # convert the json content into python data and then extract the websites
+    python_data = json.loads(json_content)
+    websites_list = [i.link for i in range(len(python_data.items))]
+
+    return websites_list
+
+def web_clawer( websites, result_num = 2 ):
+
+    '''
+        Store the HTML content of websites into a temporary file and then claw the appropriate text from it using the BeautifulSoup
+    
+        Keyword arguments:
+        websites -- the list storing the websites from Google Search results
+        result_num -- the intended result number, same as the number of pieces of text from different websites
+    '''
+
+    # original_text = []
+    for website_idx in range(result_num):
+
+        # store the website into the temporary filr
+        url = websites[website_idx]
+        header = {"User-Agent": USER_AGENT}
+        request = urllib.request.Request(url, headers = header)
+        response = urllib.request.urlopen(request).read()
+        file = open(DOC, "wb")
+        file.write(response)
+        file.close()
+
+        # fetch the appropriate text from the website
+        soup = BeautifulSoup(open(DOC), features = "lxml")
+        # for p_idx in soup.select("p"):
+        #     if len(p_idx.get_text()) > STRING_LENGTH:
+        #         original_text.append(p_idx.get_text())
+        original_text = [p_idx.get_text() for p_idx in soup.select("p") if len(p_idx.get_text()) > STRING_LENGTH]
+
+    return original_text
+
+    
