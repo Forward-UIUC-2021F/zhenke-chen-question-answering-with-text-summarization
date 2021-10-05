33d864864f389cd6168f7d5a2a0b7261b58c86ae
Alex Chen <Zhenke.18@intl.zju.edu.cn>
2021-10-06 05:57:01 +0800

    train.py and access_training_data.py added

---
+++ public/model_training/access_training_data.py
@@ -0,0 +1,6 @@
+'''
+    @author Zhenke Chen
+    @date 05/10/2021
+
+    Transfer the training data into the form which can be used by the model
+'''
--- public/model_training/train.py
+++ public/model_training/train.py
@@ -2,6 +2,55 @@
     @author Zhenke Chen
     @date 05/10/2021
 
-    Train the model
+    Construct and then train the CNN model with keras package
 '''
 
+# import the required packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import the keras package for CNN model construction
+import keras
+from keras import regularizers
+from keras.models import Sequential
+
+# define the CNN model hyperparameters
+ACTIVATION = "relu"
+
+#define the parameters for training
+EPOCHS = 100
+
+
+class TrainModel():
+
+    def construct_model():
+
+        '''
+            Construct the CNN model based on the given model hyperparameters with the keras package
+        
+            Keyword arguments:
+            
+        '''
+
+        # apply keras to construct the model
+        model = Sequential()
+
+        model.add()
+
+        model.compile()
+
+        return model
+
+
+    def train_model( model, ):
+
+        '''
+            Train the CNN model based on the given training parameters with the keras package
+        
+            Keyword arguments:
+            
+        '''
+
+        training_status = model.fit()
+
+        return training_status
