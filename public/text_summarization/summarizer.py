2cff97be0505ac8170e0bc1a725f285463f57850
Alex Chen <Zhenke.18@intl.zju.edu.cn>
2021-10-20 15:36:08 +0800

    text summarizer in progress

---
+++ public/text_summarization/summarizer.py
@@ -0,0 +1,12 @@
+'''
+    @author Zhenke Chen
+    @date 19/10/2021
+
+    Apply GPT-3 to summarize the text abstractively
+'''
+
+# import the required packages
+import numpy as np
+import os
+import openai
+
